12cbbe7a5326fb7bff5eb157360398e30387cfdc
Arelennee <helixsy06@gmail.com>
2025-03-12 21:22:28 -0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,12 +8,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyDue0pZohIyBKeR5-XL07mmiaO41-6aLlc',
-	authDomain: 'registrack-14c1e.firebaseapp.com',
-	projectId: 'registrack-14c1e',
-	storageBucket: 'registrack-14c1e.firebasestorage.app',
-	messagingSenderId: '1079839203089',
-	appId: '1:1079839203089:web:6c196deb368a06c92e36b0',
+	apiKey: 'YourApiKey',
+	authDomain: 'YourAuthDomain',
+	projectId: 'yourProjectID',
+	storageBucket: 'yourStoragefirebasestorage.app',
+	messagingSenderId: 'Env',
+	appId: 'appId',
 };
 
 // Initialize Firebase
